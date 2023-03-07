2bfd0c3f533ee08f2430b22ae52f90fc5c33c44c
annv <annv@hurasoft.com>
2023-03-08 01:12:36 +0700

    update

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Chị Nhàn';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng Chị Nhàn ';
+const contentCard = 'Chúc Chị Nhàn 8-3 luôn vui vẻ nhé';
+
+
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
